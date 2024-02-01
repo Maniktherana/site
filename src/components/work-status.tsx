30db693ea0f00d3367fbacc7a04d1495504bb13c
Manik Rana <manikrana54@gmail.com>
2024-02-01 23:11:18 +0530

    fix: should fix workstatus mobile hovercard

--- src/components/work-status.tsx
+++ src/components/work-status.tsx
@@ -8,34 +8,70 @@ import {
   HoverCardContent,
   HoverCardTrigger,
 } from "@/components/ui/hover-card";
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@/components/ui/popover";
 
 const WorkStatus = () => {
   return (
-    <HoverCard>
-      <HoverCardTrigger asChild>
-        <Button
-          variant={"link"}
-          className="flex flex-row justify-start items-center pl-0 gap-3"
-        >
-          <span className="relative flex h-3 w-3">
-            <span className="animate-ping absolute inline-flex h-full w-full rounded-full bg-emerald-500 opacity-75"></span>
-            <span className="relative inline-flex rounded-full h-3 w-3 bg-emerald-500"></span>
-          </span>
-          <p className="font-mono text-neutral-500">Open to work</p>
-        </Button>
-      </HoverCardTrigger>
-      <HoverCardContent className="text-sm text-left font-mono">
-        I&apos;m open to working on new projects. If you have something in mind,
-        feel free to{" "}
-        <Link
-          href="mailto:hello@manikrana.dev"
-          target="_blank"
-          className="underline"
-        >
-          reach out
-        </Link>
-      </HoverCardContent>
-    </HoverCard>
+    <div>
+      <div className="md:hidden">
+        <Popover>
+          <PopoverTrigger>
+            <Button
+              variant={"link"}
+              className="flex flex-row justify-start items-center pl-0 gap-3"
+            >
+              <span className="relative flex h-3 w-3">
+                <span className="animate-ping absolute inline-flex h-full w-full rounded-full bg-emerald-500 opacity-75"></span>
+                <span className="relative inline-flex rounded-full h-3 w-3 bg-emerald-500"></span>
+              </span>
+              <p className="font-mono text-neutral-500">Open to work</p>
+            </Button>
+          </PopoverTrigger>
+          <PopoverContent className="text-sm text-left font-mono">
+            I&apos;m open to working on new projects. If you have something in
+            mind, feel free to{" "}
+            <Link
+              href="mailto:hello@manikrana.dev"
+              target="_blank"
+              className="underline"
+            >
+              reach out
+            </Link>
+          </PopoverContent>
+        </Popover>
+      </div>
+      <div className="hidden md:block">
+        <HoverCard>
+          <HoverCardTrigger asChild>
+            <Button
+              variant={"link"}
+              className="flex flex-row justify-start items-center pl-0 gap-3"
+            >
+              <span className="relative flex h-3 w-3">
+                <span className="animate-ping absolute inline-flex h-full w-full rounded-full bg-emerald-500 opacity-75"></span>
+                <span className="relative inline-flex rounded-full h-3 w-3 bg-emerald-500"></span>
+              </span>
+              <p className="font-mono text-neutral-500">Open to work</p>
+            </Button>
+          </HoverCardTrigger>
+          <HoverCardContent className="text-sm text-left font-mono">
+            I&apos;m open to working on new projects. If you have something in
+            mind, feel free to{" "}
+            <Link
+              href="mailto:hello@manikrana.dev"
+              target="_blank"
+              className="underline"
+            >
+              reach out
+            </Link>
+          </HoverCardContent>
+        </HoverCard>
+      </div>
+    </div>
   );
 };
 
